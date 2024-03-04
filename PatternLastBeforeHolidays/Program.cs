83173da79049af2b5b6cbac51c0b3debe8bc653e
nghia46 <nghialoe46@gmail.com>
2024-03-04 08:43:21 +0700

    CLI

--- PatternLastBeforeHolidays/Program.cs
+++ PatternLastBeforeHolidays/Program.cs
@@ -6,16 +6,16 @@ internal abstract class Program
 {
     public static void Main()
     {
-         //new GenericFactoryClient().Main();
-         //new FactoryClient().Main();
-         //new SingletonClient().Main();
-         //new OpenCloseClient().Main();
-         //new DependencyInjectionClient().Main();
-         //new ObjectPoolClient().Main();
-         //new CommanderClient().Main();
-         //new PrototypeClient().Main();
-         //new ObserverClient().Main();
-         new StateClient().Main();
+        //new GenericFactoryClient().Main();
+        //new FactoryClient().Main();
+        //new SingletonClient().Main();
+        //new OpenCloseClient().Main();
+        //new DependencyInjectionClient().Main();
+        //new ObjectPoolClient().Main();
+        //new CommanderClient().Main();
+        //new PrototypeClient().Main();
+        //new ObserverClient().Main();
+        //new StateClient().Main();
     }
 
 }
